1627571ada00cd28e810eb35d20877eb23545a00
TropicolX <jacobsbusayo@gmail.com>
2024-06-05 15:31:13 +0100

    setting up starter branch

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,12 +14,6 @@ func main() {
 	// Load configuration
 	config.LoadConfig()
 
-	// Initialize Permify client
-	config.InitPermifyClient()
-
-	// Write Permify schema and relationships
-	config.WritePermifySchemaAndRelationships()
-
 	// Initialize the router
 	r := mux.NewRouter()
 
@@ -33,9 +27,9 @@ func main() {
 	api := r.PathPrefix("/api").Subrouter()
 	api.Use(middlewares.AuthMiddleware)
 	api.HandleFunc("/documents", controllers.UploadDocument).Methods("POST")
-	api.Handle("/documents/{id}", middlewares.ABACMiddleware("view")(http.HandlerFunc(controllers.ViewDocument))).Methods("GET")
-	api.Handle("/documents/{id}", middlewares.ABACMiddleware("edit")(http.HandlerFunc(controllers.EditDocument))).Methods("PUT")
-	api.Handle("/documents/{id}", middlewares.ABACMiddleware("delete")(http.HandlerFunc(controllers.DeleteDocument))).Methods("DELETE")
+	api.HandleFunc("/documents/{id}", controllers.ViewDocument).Methods("GET")
+	api.HandleFunc("/documents/{id}", controllers.EditDocument).Methods("PUT")
+	api.HandleFunc("/documents/{id}", controllers.DeleteDocument).Methods("DELETE")
 
 	// Start the server
 	log.Println("Starting server on :8080...")
--- internal/controllers/document_controller.go
+++ internal/controllers/document_controller.go
@@ -1,16 +1,13 @@
 package controllers
 
 import (
-	"context"
 	"encoding/json"
-	"golang-abac-demo/internal/config"
 	"golang-abac-demo/internal/models"
 	"golang-abac-demo/internal/utils"
 	"log"
 	"net/http"
 	"strconv"
 
-	v1 "github.com/Permify/permify-go/generated/base/v1"
 	"github.com/gorilla/mux"
 )
 
@@ -30,58 +27,26 @@ func UploadDocument(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	doc.OwnerID = user.ID                              // Set document owner as the user who uploaded it
-	doc.ID = strconv.Itoa((len(models.Documents) + 1)) // Generate document ID
+	doc.OwnerID = user.ID
+	noOfDocs := len(models.Documents)
+
+	if noOfDocs == 0 {
+		doc.ID = "1"
+	} else {
+		lastDocIndex := noOfDocs - 1
+		lastDocID, err := strconv.Atoi(models.Documents[lastDocIndex].ID)
+
+		if err != nil {
+			log.Printf("Failed to fetch last document ID: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		doc.ID = strconv.Itoa(lastDocID + 1)
+	}
 
 	// Add document to repository (this would be replaced with actual DB call)
 	models.AddDocument(doc)
-
-	// Add document to Permify
-	tuples := []*v1.Tuple{{
-		Entity: &v1.Entity{
-			Type: "document",
-			Id:   doc.ID,
-		},
-		Relation: "owner",
-		Subject: &v1.Subject{
-			Type: "user",
-			Id:   user.ID,
-		},
-	}}
-
-	attributes := []*v1.Attribute{
-		{
-			Entity: &v1.Entity{
-				Type: "document",
-				Id:   doc.ID,
-			},
-			Attribute: "classification",
-			Value:     config.ConvertStringToAny(doc.Classification),
-		},
-		{
-			Entity: &v1.Entity{
-				Type: "document",
-				Id:   doc.ID,
-			},
-			Attribute: "department",
-			Value:     config.ConvertStringToAny(user.Department),
-		},
-	}
-
-	_, err = config.PermifyClient.Data.Write(context.Background(), &v1.DataWriteRequest{
-		TenantId: "t1",
-		Metadata: &v1.DataWriteRequestMetadata{
-			SchemaVersion: config.SchemaVersion,
-		},
-		Tuples:     tuples,
-		Attributes: attributes,
-	})
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(map[string]string{"error": "Failed to write relationship to Permify"})
-		return
-	}
 
 	utils.InfoLogger.Printf("User '%s' uploaded document %s", user.Username, doc.ID)
 
@@ -154,30 +119,6 @@ func DeleteDocument(w http.ResponseWriter, r *http.Request) {
 
 	// Delete document from repository (this would be replaced with actual DB call)
 	models.DeleteDocument(docID)
-
-	// Delete document from Permify
-	_, err = config.PermifyClient.Data.Delete(context.Background(), &v1.DataDeleteRequest{
-		TenantId: "t1",
-		TupleFilter: &v1.TupleFilter{
-			Entity: &v1.EntityFilter{
-				Type: "document",
-				Ids:  []string{doc.ID},
-			},
-		},
-		AttributeFilter: &v1.AttributeFilter{
-			Entity: &v1.EntityFilter{
-				Type: "document",
-				Ids:  []string{doc.ID},
-			},
-			Attributes: []string{"classification", "department"},
-		},
-	})
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(map[string]string{"error": "Failed to delete document from Permify"})
-		return
-	}
 
 	utils.InfoLogger.Printf("User '%s' deleted document %s", claims.Username, doc.ID)
 
